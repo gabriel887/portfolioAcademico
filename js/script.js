961542b5ffa9e466ae92a924c04c05e6778daea1
gabriel887 <gabriel_valentim17@hotmail.com>
2023-09-10 17:50:14 -0300

    sei la

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 $(document).ready(function (){
 
     $(".item-navbar").click(trocaPagina);
+    $(".dropdownMenu").click(mexeDropDown);
 });
 
 
@@ -21,6 +22,22 @@ function trocaPagina(){
         }
     });
     $(this).addClass("selected");
+}
+
+function mexeDropDown(){
+    var label = $(this).find("label");
+    var i = label.find("i").first();
+    if(i.hasClass("fa-caret-right")){
+        i.addClass("fa-sort-down");
+        i.removeClass("fa-caret-right");
+        $(this).addClass("dropped");
+        $(this).addClass("opened");
+    } else {
+        i.removeClass("fa-sort-down");
+        i.addClass("fa-caret-right");
+        $(this).removeClass("dropped");
+        $(this).removeClass("opened");
+    }
 }
 
 //fa-caret-right seta pra direita
