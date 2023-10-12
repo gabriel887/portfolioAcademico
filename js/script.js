9e320eb802d11b309431cf50f7a233330bfb6b23
gabriel887 <gabriel_valentim17@hotmail.com>
2023-10-12 20:39:45 -0300

    Faazer o editor-fold funcionar

--- js/script.js
+++ js/script.js
@@ -4,7 +4,10 @@ $(document).ready(function (){
     $(".item-navbar").click(trocaPagina);
     $(".dropdownMenu > label").click(mexeDropDown);
     $(".menuPrincipal").click(mostraMenuPrincipal);
+    $(".setaBaixoHide").click(toggleEditorFoldSetaBaixo);
+    $(".code").on("click", ".textoFolded", toggleEditorFoldTextoFolded);
 
+    popularNumeracaoAba();
     popularNumeracao();
 });
 
@@ -63,7 +66,7 @@ function mostraMenuPrincipal(){
 
 function popularNumeracao(){
     let i = 1;
-    $(".number").each(function(){
+    $("#mainBemVindo .number").each(function(){
         let divNumber = $(this);
         if(divNumber.parent().css("display") !== 'none'){
             divNumber.text(i);
@@ -72,6 +75,61 @@ function popularNumeracao(){
     });
 }
 
+function popularNumeracaoAba(){
+    let i = 1;
+    $("#biografia .number").each(function(){
+        let divNumber = $(this);
+        if(divNumber.parent().css("display") !== 'none'){
+            divNumber.append("<label>" + i + "</label>");
+            i++;
+        }
+        if(divNumber.hasClass("setaCimaHide")){
+            divNumber.append(`<i class="fa-solid fa-angle-up iconesSetas"> </i>`); 
+        }
+        if(divNumber.hasClass("setaBaixoHide")){
+            divNumber.append(`<i class="fa-solid fa-angle-down iconesSetas"> </i>`); 
+        }
+    });
+}
+
+function toggleEditorFoldSetaBaixo() {
+    let hasClassEscondido = $(this).hasClass("escondido");
+    if(hasClassEscondido){
+        $(this).removeClass("escondido");
+        var divSetaCima = $(this).parent().nextAll(':has(.setaCimaHide):first');
+        $(this).parent().nextUntil(divSetaCima).add(divSetaCima).show();
+        let texto = $(this).next().text();
+        $(this).next().html(`<label class="comentario">&lt;editor-fold desc="` + texto + ` "&gt;</label> `);
+    }else{
+        $(this).addClass("escondido");
+        var divSetaCima = $(this).parent().nextAll(':has(.setaCimaHide):first');
+        $(this).parent().nextUntil(divSetaCima).add(divSetaCima).hide();
+        let texto = $(this).next().text();
+        texto = texto.substr(texto.indexOf('"')+1);
+        texto = texto.substr(0,texto.indexOf('"'));
+        $(this).next().html(`<label class="comentario textoFolded">` + texto + `</label> `);
+    }
+}
+
+function toggleEditorFoldTextoFolded() {
+    let divNumber = $(this).parent().prev();
+    let hasClassEscondido = divNumber.hasClass("escondido");
+    if(hasClassEscondido){
+        divNumber.removeClass("escondido");
+        var divSetaCima = divNumber.parent().nextAll(':has(.setaCimaHide):first');
+        divNumber.parent().nextUntil(divSetaCima).add(divSetaCima).show();
+        let texto = divNumber.next().text();
+        divNumber.next().html(`<label class="comentario">&lt;editor-fold desc="` + texto + ` "&gt;</label> `);
+    }else{
+        divNumber.addClass("escondido");
+        var divSetaCima = divNumber.parent().nextAll(':has(.setaCimaHide):first');
+        divNumber.parent().nextUntil(divSetaCima).add(divSetaCima).hide();
+        let texto = divNumber.next().text();
+        texto = texto.substr(texto.indexOf('"')+1);
+        texto = texto.substr(0,texto.indexOf('"'));
+        divNumber.next().html(`<label class="comentario textoFolded">` + texto + `</label> `);
+    }
+}
 //fa-caret-right seta pra direita
 //sort-down seta pa baxo
 // &nbsp; espaço
